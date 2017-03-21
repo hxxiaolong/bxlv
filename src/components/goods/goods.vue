4a57595aa853c3518acf62ca2401057a3e3e691f
hx小龙 <864739024@qq.com>
2017-03-21 19:02:27 +0800

    加上生产环境的vue模板

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="goods">
-    <div class="goodsleft">dd</div>
+    <div class="goodsleft">
+      <div class="goods_item onepxbottom">ddd1</div>
+      <div class="goods_item">ddd2</div>
+      <div class="goods_item">ddd3</div>
+    </div>
     <div class="goodsright">ddd</div>
   </div>
 </template>
@@ -10,13 +14,26 @@
 </script>
 
 <style lang="scss" rel="stylesheet/scss">
+  @import "../../common/style/util";
+
   .goods{
     display: flex;
     .goodsleft{
       flex:0 0 80px;
+      height: 280px;
       width: 80px;
-      height: 100%;
       background-color: red;
+      display: flex;
+      flex-direction: column;
+      .goods_item{
+        position: relative;
+        text-align: center;
+        height: 20px;
+        line-height: 20px;
+      }
+      .onepxbottom:after{
+        border-bottom: 1px solid blue;
+      }
     }
     .goodsright{
       width: 100%;
